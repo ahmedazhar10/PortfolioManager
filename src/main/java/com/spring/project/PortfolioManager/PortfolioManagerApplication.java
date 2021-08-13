975d855e54ddef01dcca5a1f927c9a055f946f6c
Yiheng <lluuyyiihh@icloud.com>
2021-08-13 09:49:55 -0400

    first commit

---
+++ src/main/java/com/spring/project/PortfolioManager/PortfolioManagerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.project.PortfolioManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/project/PortfolioManager/PortfolioManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.project.PortfolioManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
